a30a1789c5b1994287fb71b8a15afd09ba7b51d3
Ruoyu Wang <86712051+RuoyuWang-MS@users.noreply.github.com>
2022-09-07 15:45:40 +0800

    Upgrade JS SDK to 2.0.0  (#7)
    * init JS SDK 2.0.0
    
    * try release1
    
    * fix list deployments bug
    
    * remove useless code && modify test
    
    * remove node module and lib
    
    * revert two files
    
    * remove useless comments

---
+++ __tests__/DeploymentProvider/DeploymentHelper.test.ts
@@ -0,0 +1,41 @@
+import * as asa from '@azure/arm-appplatform'
+import * as identity from '@azure/identity'
+import {ActionParameters, ActionParametersUtility} from "../../src/operations/ActionParameters";
+import {DeploymentHelper} from "../../src/DeploymentProvider/DeploymentHelper";
+
+jest.mock('@azure/identity');
+jest.mock('@actions/core');
+
+describe('Test azure-spring-apps-deployment-helper', () => {
+    afterEach(() => {
+        jest.restoreAllMocks();
+    })
+    let clientMock: jest.Mocked<asa.AppPlatformManagementClient> = new asa.AppPlatformManagementClient(new identity.DefaultAzureCredential(), '') as any;
+    let paramsMock: jest.Mocked<ActionParameters> = {} as any;
+    let deploymentListMock: Array<asa.DeploymentResource> = [
+        {
+            properties: {
+                active: false
+            },
+            name: 'staging'
+        },
+        {
+            properties: {
+                active: true
+            },
+            name: 'production'
+        }
+    ];
+    let responseMock: asa.DeploymentsListResponse = deploymentListMock as asa.DeploymentsListResponse
+    clientMock.deployments.list = jest.fn().mockReturnValue(responseMock);
+    test("get staging deployment name", async () => {
+        const stagingName = await DeploymentHelper.getStagingDeploymentName(clientMock, paramsMock);
+        expect(stagingName).toBe('staging');
+    });
+
+    test("gets all deployment names", async () => {
+        const names = await DeploymentHelper.getAllDeploymentsName(clientMock, paramsMock);
+        expect(names).toMatchObject(['staging', 'production']);
+    });
+
+});
